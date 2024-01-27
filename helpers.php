8db9048ed1ee7c26d895a8e5a35f61ab50ac800b
Rui Marques <a.marques1967@gmail.com>
2024-01-27 22:31:22 +0000

    Folder structure refactor

--- helpers.php
+++ helpers.php
@@ -21,7 +21,7 @@ function basePath(string $path = ''): string
  */
 function loadView(string $name, array $data = []): void
 {
-    $viewPath = basePath("views/{$name}.view.php");
+    $viewPath = basePath("App/views/{$name}.view.php");
 
     if (file_exists($viewPath)) {
         extract($data);
@@ -40,7 +40,7 @@ function loadView(string $name, array $data = []): void
  */
 function loadPartial(string $name): void
 {
-    $partialPath = basePath("views/partials/{$name}.php");
+    $partialPath = basePath("App/views/partials/{$name}.php");
 
     if (file_exists($partialPath)) {
         require $partialPath;
