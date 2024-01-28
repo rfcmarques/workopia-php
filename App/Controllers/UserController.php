37265dd9a9de6b3881f6792e022900fcd10f2103
Rui Marques <a.marques1967@gmail.com>
2024-01-28 18:32:41 +0000

    Created store function

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -24,4 +24,46 @@ class UserController
     {
         loadView('users/create');
     }
+
+    public function store()
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+        $password = $_POST['password'];
+        $passwordConfirmation = $_POST['password_confirmation'];
+
+        $errors = [];
+
+        if (!Validation::email($email)) {
+            $errors['email'] = 'Please enter a vlida email address';
+        }
+
+        if (!Validation::string($name, 2, 50)) {
+            $errors['name'] = 'Name must be between 2 and 50 characters';
+        }
+
+        if (!Validation::string($password, 6)) {
+            $errors['pasword'] = 'Pasword must be at least 6 characters';
+        }
+
+        if (!Validation::match($password, $passwordConfirmation)) {
+            $errors['password_confirmation'] = 'Passwords do not match';
+        }
+
+        if (!empty($errors)) {
+            loadView('users/create', [
+                'errors' => $errors,
+                'user' => [
+                    'name' => $name,
+                    'email' => $email,
+                    'city' => $city,
+                    'state' => $state
+                ]
+            ]);
+            exit;
+        } else {
+        }
+    }
 }
