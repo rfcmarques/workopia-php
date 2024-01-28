a6fe65463559b1e4e039dce70e80bdda29cfef18
Rui Marques <a.marques1967@gmail.com>
2024-01-28 16:26:55 +0000

    Changed route function to delete with more http methods

--- Framework/Router.php
+++ Framework/Router.php
@@ -92,6 +92,11 @@ class Router
     {
         $requestMethod = $_SERVER['REQUEST_METHOD'];
 
+        // Check for _method input
+        if ($requestMethod === 'POST' && isset($_POST['_method'])) {
+            $requestMethod = strtoupper($_POST['_method']);
+        }
+
         foreach ($this->routes as $route) {
             // Split the current URI into segments
             $uriSegments = explode('/', trim($uri, '/'));
