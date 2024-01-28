af291c1dd50c6c4714e501b5ffc4a8f5ecefd5a9
Rui Marques <a.marques1967@gmail.com>
2024-01-28 22:42:50 +0000

    Created Authorization class for validating ownership

---
+++ Framework/Authorization.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Framework;
+
+use Framework\Session;
+
+class Authorization
+{
+    public static function isOwner($resourceId)
+    {
+        $sessionUser = Session::get('user');
+
+        if ($sessionUser !== null && isset($sessionUser['id'])) {
+            $sessionUserId = (int) $sessionUser['id'];
+
+            return $sessionUserId === $resourceId;
+        }
+
+        return false;
+    }
+}
