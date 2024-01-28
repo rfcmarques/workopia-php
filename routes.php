a6e5ac4be6672d19a3d418afd4bf5ce6fc6750bd
Rui Marques <a.marques1967@gmail.com>
2024-01-28 17:46:48 +0000

    Added edit listing routes

--- routes.php
+++ routes.php
@@ -6,4 +6,6 @@ $router->get('/listings', 'ListingController@index');
 $router->post('/listings', 'ListingController@store');
 $router->get('/listings/create', 'ListingController@create');
 $router->get('/listings/{id}', 'ListingController@show');
+$router->get('/listings/edit/{id}', 'ListingController@edit');
+$router->put('/listings/{id}', 'ListingController@update');
 $router->delete('/listings/{id}', 'ListingController@destroy');
