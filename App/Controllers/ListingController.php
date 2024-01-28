0669bbc9604cdb579fdb8298bde55d9de64dcb11
Rui Marques <a.marques1967@gmail.com>
2024-01-28 17:47:44 +0000

    Created edit listing function

--- App/Controllers/ListingController.php
+++ App/Controllers/ListingController.php
@@ -123,4 +123,22 @@ class ListingController
 
         redirect('/listings');
     }
+
+    public function edit($params)
+    {
+        $id = $params['id'] ?? '';
+
+        $params = ['id' => $id];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            die;
+        }
+
+        loadView('listings/edit', [
+            'listing' => $listing
+        ]);
+    }
 }
