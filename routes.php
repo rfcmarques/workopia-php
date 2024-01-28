f2d54e774b2e42e345a94c128ab8be8676d37e10
Rui Marques <a.marques1967@gmail.com>
2024-01-28 15:39:54 +0000

    Added post route for listings

--- routes.php
+++ routes.php
@@ -1,6 +1,8 @@
 <?php
 
 $router->get('/', 'HomeController@index');
+
 $router->get('/listings', 'ListingController@index');
+$router->post('/listings', 'ListingController@store');
 $router->get('/listings/create', 'ListingController@create');
 $router->get('/listing/{id}', 'ListingController@show');
