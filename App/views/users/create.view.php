e10638418eee3da57d0ed383d17122b826cfffff
Rui Marques <a.marques1967@gmail.com>
2024-01-28 18:33:04 +0000

    Changed validation errors to partial

--- App/views/users/create.view.php
+++ App/views/users/create.view.php
@@ -5,9 +5,11 @@
     <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-500 mx-6">
         <h2 class="text-4xl text-center font-bold mb-4">Register</h2>
 
-        <?= loadPartial('message') ?>
+        <?= loadPartial('errors', [
+            'errors' => $errors ?? []
+        ]) ?>
 
-        <form>
+        <form method="POST" action="/auth/register">
             <div class="mb-4">
                 <input type="text" name="name" placeholder="Full Name" class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
